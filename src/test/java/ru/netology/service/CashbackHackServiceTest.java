48ad17089b5371abbce6e70fb6f7460ef99ce66d
Paulina Miller <paulina25miller@gmail.com>
2024-10-22 15:38:48 +0300

    Expected and actual values changed in tests

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -13,7 +13,7 @@ public class CashbackHackServiceTest {
             int actual = service.remain(amount);
             int expected = 100;
 
-            assertEquals(actual, expected);
+            assertEquals(expected, actual);
 
     }
 
@@ -26,7 +26,7 @@ public class CashbackHackServiceTest {
         int actual = service.remain(amount);
         int expected = 0;
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
 
     }
 
@@ -38,7 +38,7 @@ public class CashbackHackServiceTest {
         int actual = service.remain(amount);
         int expected = 500;
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
 
     }
 }
